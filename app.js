456bd7112b49ddc6ed76ef1637498191d189535c
yohan <rafael.hilario@oarenta.local>
2021-09-09 17:49:19 -0400

    Agregando Cards

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
@@ -6,10 +9,19 @@ const fetchData = async () => {
     try {
         const res = await fetch('api.json')
         const data = await res.json()
-        console.log(data)
+        //console.log(data)
+        pintarCards(data)
     } catch (error) {
         console.log(error)
     }
 }
  
-console.log("App JS")
+const pintarCards = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title
+
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    });
+    items.appendChild(fragment)
+}
